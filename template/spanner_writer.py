6fcfdb84e19bc6e6e5965ba118aa7b43df235561
bharath <bharathts07@gmail.com>
2020-09-03 08:38:10 +0900

    Update dummy spanner writer code with direct runner

--- template/spanner_writer.py
+++ template/spanner_writer.py
@@ -5,7 +5,7 @@ import apache_beam as beam
 from google.cloud import spanner
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
-from apache_beam.io import iobase
+from apache_beam.io import iobase, WriteToText
 
 
 def runDataflow(argv=None, save_main_session=True):
@@ -13,13 +13,15 @@ def runDataflow(argv=None, save_main_session=True):
     known_args, pipeline_args = parser.parse_known_args(argv)
 
     pipeline_args.extend([
-        '--runner=DataFlowRunner',
+        '--runner=DirectRunner',
+        # '--runner=DataflowRunner',
         '--project=kouzoh-p-bharath',
         '--staging_location=gs://kouzoh-p-bharath-beam/staging',
         '--temp_location=gs://kouzoh-p-bharath-beam/temp',
         '--job_name=syncer-bq-spanner',
-        '--requirements_file=requirements.txt',
-        '--num_workers=10',
+        # '--requirements_file=requirements.txt',
+        '--num_workers=1',
+        # '--region=us-central1',
     ])
 
     pipeline_options = PipelineOptions(pipeline_args)
@@ -27,20 +29,24 @@ def runDataflow(argv=None, save_main_session=True):
 
     sql_stmt = 'select user_id,is_registeration_completed,is_profile_completed,' \
                'is_list_completed,is_sold_completed,is_liked_completed,is_ss_completed,' \
-               'is_buy_completed,is_fta_completed from [kouzoh-p-bharath:achievements.achieve_v2] LIMIT 100;'
+               'is_buy_completed,is_fta_completed from [kouzoh-p-bharath:achievements.achieve_v2] LIMIT 10;'
 
     def makeRow(element):
         (user_id, register, profile, listed, sold, liked, savedsearch, bought, fta) = element
         return {
-            "user_id": user_id, "register": register, "profile": profile, "listed": listed, "sold": sold,
-            "liked": liked, "savedsearch": savedsearch, "bought": bought, "fta": fta,
+            "user_id": int(user_id), "register": int(register), "profile": int(profile),
+            "listed": int(listed), "sold": int(sold), "liked": int(liked), "savedsearch": int(savedsearch),
+            "bought": int(bought), "fta": int(fta),
         }
 
     cols = ['user_id', 'is_buy_completed', 'is_fta_completed', 'is_liked_completed', 'is_list_completed',
-            'is_profile_completed', 'is_registeration_completed', 'is_sold_completed', 'is_ss_completed']
+            'is_profile_completed', 'is_registration_completed', 'is_sold_completed', 'is_ss_completed']
 
     with beam.Pipeline(options=pipeline_options) as p:
-        source = p | 'Collecting' >> beam.io.Read(beam.io.BigQuerySource(query=sql_stmt))
+        source = [
+            (1, 1, 1, 1, 1, 1, 1, 1, 1),
+            (2, 0, 1, 1, 1, 1, 1, 1, 0),
+        ]
 
         source | "Writing to spanner" >> beam.io.Write(SimpleKVSink(table_name="achieve",
                                                                     project="kouzoh-p-bharath",
@@ -62,7 +68,7 @@ class SimpleKVSink(iobase.Sink):
         self.columns = columns
 
     def initialize_write(self):
-        client = spanner.Client(project=self.project, admin=True)
+        client = spanner.Client(project=self.project)
         instance = client.instance(self.instance_id)
 
     def open_writer(self, table, uid):
