61ef7c6793f4b394b45b0301c581f70d527fc959
bharath <bharathts07@gmail.com>
2020-09-02 19:16:48 +0900

    Update code for dataflow

--- spanner/README.md
+++ spanner/README.md
@@ -12,3 +12,9 @@ export GOOGLE_APPLICATION_CREDENTIALS=<PATH TO JSON CREDS>
 pipenv run python -m spanner.create
 pipenv run python -m spanner.insert
 ```
+
+For requirements.txt
+```shell script
+pipenv sync
+pipenv run pip freeze
+```
--- spanner/create.py
+++ spanner/create.py
@@ -2,24 +2,46 @@ from google.cloud import spanner
 import logging
 
 
+def create_instance(instance_id):
+    """Creates an instance."""
+    spanner_client = spanner.Client()
+
+    config_name = "{}/instanceConfigs/regional-us-central1".format(
+        spanner_client.project_name
+    )
+
+    instance = spanner_client.instance(
+        instance_id,
+        configuration_name=config_name,
+        display_name="test",
+        node_count=1,
+    )
+
+    operation = instance.create()
+
+    print('Waiting for operation to complete...')
+    operation.result(120)
+
+    print('Created instance {}'.format(instance_id))
+
+
 def create_database(instance_id, database_id):
     """Creates a database and tables for sample data."""
     spanner_client = spanner.Client()
     instance = spanner_client.instance(instance_id)
 
     database = instance.database(database_id, ddl_statements=[
-        """CREATE TABLE Singers (
-            SingerId     INT64 NOT NULL,
-            FirstName    STRING(1024),
-            LastName     STRING(1024),
-            SingerInfo   BYTES(MAX)
-        ) PRIMARY KEY (SingerId)""",
-        """CREATE TABLE Albums (
-            SingerId     INT64 NOT NULL,
-            AlbumId      INT64 NOT NULL,
-            AlbumTitle   STRING(MAX)
-        ) PRIMARY KEY (SingerId, AlbumId),
-        INTERLEAVE IN PARENT Singers ON DELETE CASCADE"""
+        """CREATE TABLE achieve (
+            user_id                     INT64 NOT NULL,
+            is_buy_completed            INT64 NOT NULL,
+            is_fta_completed            INT64 NOT NULL,
+            is_liked_completed          INT64 NOT NULL,
+            is_list_completed           INT64 NOT NULL,
+            is_profile_completed        INT64 NOT NULL,
+            is_registration_completed   INT64 NOT NULL,
+            is_sold_completed           INT64 NOT NULL,
+            is_ss_completed             INT64 NOT NULL,
+        ) PRIMARY KEY (user_id)"""
     ])
 
     operation = database.create()
@@ -33,4 +55,5 @@ def create_database(instance_id, database_id):
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-    create_database("test", "dummy2")
+    create_instance("test")
+    create_database("test", "dummy")
